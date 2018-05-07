2f32816dff2dfe025c83d29132616488d5650d65
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-05-07 14:53:42 -0400

    Updated to address user and ask for name.

--- Dr_Saunders.py
+++ Dr_Saunders.py
@@ -1,1 +1,4 @@
 # Dr Saunders
+name = input("Hello, my name is Dr. Saunders. Thank you for joining me today. What is your name?\n")
+
+input("It's nice to meet you, {}. Tell me, how are you feeling today?\n".format(name))
