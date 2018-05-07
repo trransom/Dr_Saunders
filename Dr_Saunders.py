aa2d7cb4e5d0a31fb1c20e6c19d28711374b5ae8
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-05-07 15:50:36 -0400

    Basic functionality of 'depressed' installed

--- Dr_Saunders.py
+++ Dr_Saunders.py
@@ -1,4 +1,15 @@
+import os
+import re
+
 # Dr Saunders
 name = input("Hello, my name is Dr. Saunders. Thank you for joining me today. What is your name?\n")
+#TODO: Only grab first name from answer, leaving off any punctuation.
+response = re.compile(input("It's nice to meet you, {}. Tell me, how are you feeling today?\n".format(name)))
 
-input("It's nice to meet you, {}. Tell me, how are you feeling today?\n".format(name))
+
+#('.* (I\'m|I am) (depressed|sad) .*')
+if response.search('((I am|I\'m)+ depressed)'):
+    print("I am sorry that you are depressed.")
+    
+
+
